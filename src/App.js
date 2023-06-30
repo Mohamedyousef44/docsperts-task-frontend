e1bc6d75cb7fa2d75ac36ec9a6610426a3c049f2
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-07-01 00:14:52 +0300

    create routes for all pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './Components/Login';
+import Signup from './Components/Signup';
+import NotFound from './Components/NotFound';
+import Home from './Components/Book/Home';
+import Layout from './Components/Layout';
+import BookDetails from './Components/Book/BookDetails';
+import BookEdit from './Components/Book/BookEdit';
+import NewBook from './Components/Book/NewBook';
+import Pages from './Components/Page/Pages';
+import PageDetails from './Components/Page/PageDetails'
+import EditPage from './Components/Page/EditPage';
+import NewPage from './Components/Page/NewPage';
+import NotAuthorized from './Components/NotAuthorized';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Routes>
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Signup />} />
+      <Route path='*' element={<NotFound />} />
+      <Route path='/notAuthorized' element={<NotAuthorized />} />
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path='/books/new' element={<NewBook />} />
+        <Route path='/books/:id' element={<BookDetails />} />
+        <Route path='/books/:id/edit' element={<BookEdit />} />
+        <Route path='/books/:id/page' element={<Pages />} />
+        <Route path='/books/:id/page/:pk' element={<PageDetails />} />
+        <Route path='/books/:id/page/new' element={<NewPage />} />
+        <Route path='/books/:id/page/:pk/edit' element={<EditPage />} />
+      </Route>
+    </Routes>
+    </>
+      
   );
 }
 
